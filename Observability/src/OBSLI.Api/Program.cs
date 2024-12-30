fea6689f0e414a8d185652adc3ac175b47e98520
Andrey Dario <andreydario26@gmail.com>
2024-12-29 22:59:27 -0300

    Configurando Elastsearch e Kibana - Feature

--- Observability/src/OBSLI.Api/Program.cs
+++ Observability/src/OBSLI.Api/Program.cs
@@ -1,3 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+using OBSI.Infra;
+using OBSI.Infra.Repository;
+using OBSI.Infra.RepositoryImp;
+using Serilog.Formatting.Compact;
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +13,23 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<ProdutosContext>(options =>
+              options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+var logger = new LoggerConfiguration()
+                    .ReadFrom.Configuration(builder.Configuration)
+                    .Enrich.FromLogContext()
+                    .WriteTo.Console(new CompactJsonFormatter())
+                    .WriteTo.Elasticsearch(new Serilog.Sinks.Elasticsearch.ElasticsearchSinkOptions(new Uri("http://localhost:9200"))
+                    {
+                        AutoRegisterTemplate = true,
+                    })
+                    .CreateLogger();
+builder.Host.UseSerilog(logger);
+
+builder.Services.AddScoped<IFornecedor, FornecedorRepository>();
+
+
 
 var app = builder.Build();
 
@@ -15,6 +39,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseSerilogRequestLogging();
+
 
 app.UseHttpsRedirection();
 
